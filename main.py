ce53500991a0f0c51c49a3914b2aeb7c0354de14
tripleseven <haddo84@gmail.com>
2024-08-22 15:31:25 -0700

    ch2 1: modules

---
+++ main.py
@@ -0,0 +1,13 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+from constants import *
+
+def main():
+	print( "Starting asteroids!")
+	print(f"Screen width: {SCREEN_WIDTH}")
+	print(f"Screen heigh: {SCREEN_HEIGHT}")
+
+if __name__ == "__main__":
+    main()
