0757cce8b9d64515d4b387b8d808c3c8485be065
tripleseven <haddo84@gmail.com>
2024-08-23 07:57:51 -0700

    ch 2 lesson 3

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ from constants import *
 def main():
      pygame.init()
      screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+     clock = pygame.time.Clock()
+     dt = 0
 
      while True:
             for event in pygame.event.get():
@@ -16,6 +18,7 @@ def main():
                             return
             screen.fill((0,0,0))
             pygame.display.flip()
+            dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
