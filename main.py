809f7aaf44f369dec73cc74971cc954016c8ff26
tripleseven <haddo84@gmail.com>
2024-08-27 07:28:33 -0700

    ch 2 lesson 8

--- main.py
+++ main.py
@@ -11,6 +11,11 @@ def main():
      pygame.init()
      screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
      clock = pygame.time.Clock()
+     updatable = pygame.sprite.Group()
+     drawable = pygame.sprite.Group()
+
+     Player.containers = (updatable, drawable)
+
      player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
      dt = 0
 
@@ -18,9 +23,14 @@ def main():
             for event in pygame.event.get():
                     if event.type == pygame.QUIT:
                             return
-            player.update(dt)
+            for obj in updatable:
+                  obj.update(dt)
+
             screen.fill("black")
-            player.draw(screen)
+
+            for obj in drawable:
+                  obj.draw(screen)
+
             pygame.display.flip()
             
             dt = clock.tick(60) / 1000
